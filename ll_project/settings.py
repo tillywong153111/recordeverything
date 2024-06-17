17fa3c08e5c589c50b67c1ca057a4b3bab10a9f7
Tillywong <tillywong15@gmail.com>
2024-06-17 12:21:58 +0800

    Update settings.py

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mibxt7sboxg%r)9t6z=a6@weyo(thgbknfcwgyowgx9$5s4&=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =ALLOWED_HOSTS = ['recordeverything-production.up.railway.app']
 
 # Application definition
 
